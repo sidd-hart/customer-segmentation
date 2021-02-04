477cf7e8abd42f3ebf0f01a47ba03065d2be8c9e
Siddhartha Kala <s.kala1@hotmail.com>
2021-02-04 09:04:27 +0530

    This commit will add everything right before starting sales prediction and purchase analytics

--- .virtual_documents/notebooks/2.2-LifeTimes-CLTV-Analysis.ipynb.py
+++ .virtual_documents/notebooks/2.2-LifeTimes-CLTV-Analysis.ipynb.py
@@ -85,34 +85,96 @@ print(sum(data['frequency'] == 0)/float(len(data)))
 from lifetimes import BetaGeoFitter
 
 
-bgf = BetaGeoFitter
+bgf = BetaGeoFitter(penalizer_coef=0.0)
 
 
+bgf.fit(data['frequency'], data['recency'], data['T'])
+print(bgf)
 
 
+from lifetimes.plotting import plot_frequency_recency_matrix
 
 
+fig = plt.figure(figsize=(12, 8))
+plot_frequency_recency_matrix(bgf);
 
 
+from lifetimes.plotting import plot_probability_alive_matrix
 
 
+plot_probability_alive_matrix(bgf);
 
 
+t = 1
+data['predicted_purchases'] = bgf.conditional_expected_number_of_purchases_up_to_time(
+    t, 
+    data['frequency'], 
+    data['recency'],
+    data['T']
+)
 
 
+data.sample(5)
 
 
+data.sort_values(by='predicted_purchases').tail(5)
 
 
+from lifetimes.plotting import plot_period_transactions
 
 
+plot_period_transactions(bgf);
 
 
+from lifetimes.utils import calibration_and_holdout_data
 
 
+summary_cal_holdout = calibration_and_holdout_data(
+    df, 
+    'CustomerID', 
+    'InvoiceDate', 
+    calibration_period_end='2011-06-08',
+    observation_period_end='2011-12-9'
+)
 
 
+display(summary_cal_holdout.head())
 
+
+from lifetimes.plotting import plot_calibration_purchases_vs_holdout_purchases
+
+
+bgf.fit(summary_cal_holdout['frequency_cal'], 
+        summary_cal_holdout['recency_cal'], summary_cal_holdout['T_cal'])
+
+
+plot_calibration_purchases_vs_holdout_purchases(bgf, summary_cal_holdout);
+
+
+t = 10
+individual = data.loc[12347]
+
+
+bgf.predict(t, individual['frequency'], individual['recency'], individual['T'])
+
+
+from lifetimes.plotting import plot_history_alive
+
+
+id = 14606
+days_since_birth = 365
+sp_trans = df.loc[df['CustomerID'] == id]
+
+
+with plt.style.context('seaborn'):
+    plot_history_alive(bgf, days_since_birth, sp_trans, 'InvoiceDate');
+
+
+id = 14720
+days_since_birth = 365
+sp_trans = df.loc[df['CustomerID'] == id]
+with plt.style.context('seaborn'):
+    plot_history_alive(bgf, days_since_birth, sp_trans, 'InvoiceDate');
 
 
 
